d901da202d990e3e44883fa41f5954c7d89a2a1b
Sedat Aktas <sedata38@gmail.com>
2020-10-29 15:35:35 +0300

    Implemented Find PI to the Nth Digit

---
+++ numbers/pi.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	MaxDigit = 20
+)
+
+func main() {
+	fmt.Println("Enter Nth Digit:")
+
+	var digit int
+	_, err := fmt.Scanf("%d", &digit)
+	if err != nil {
+		panic(err)
+	}
+
+	if digit > 0 && digit < MaxDigit {
+		fmt.Printf("%.*f\n", digit, math.Pi)
+	} else {
+		fmt.Println("Digit number should be between 0 and 20")
+	}
+}
